34314268eaabe963ef3d30962319b89c2638573b
Erica Little <eteney90@gmail.com>
2021-04-05 22:25:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,12 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-
+    quantity.times do
+      @items << title
+    end
+    @last_total = @total
+    @total += price * quantity  
+    end
   end
 
 end
